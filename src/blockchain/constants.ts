4b6e4038861f3ea3b441b85a95ded10a8cad8217
Røbėrtø <106074508+EchoDex@users.noreply.github.com>
2024-06-11 16:56:48 +0400

    fix: correct type in contracts

--- src/blockchain/constants.ts
+++ src/blockchain/constants.ts
@@ -6,7 +6,7 @@ import { Token } from "@src/entity/Token";
 import TOKENS_JSON from "./tokens.json";
 
 export const CONTRACT_ADDRESSES: OrderbookContracts = {
-  spotMarket: "0x4a2ce054e3e94155f7092f7365b212f7f45105b74819c623744ebcc5d065c6ac",
+  market: "0x4a2ce054e3e94155f7092f7365b212f7f45105b74819c623744ebcc5d065c6ac",
   tokenFactory: "0x3141a3f11e3f784364d57860e3a4dcf9b73d42e23fd49038773cefb09c633348",
   pyth: "0x3cd5005f23321c8ae0ccfa98fb07d9a5ff325c483f21d2d9540d6897007600c9",
 };
