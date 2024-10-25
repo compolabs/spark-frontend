f2d64aa0725aff9272478a1ecb0721fcd628de26
Røbėrtø <106074508+EchoDex@users.noreply.github.com>
2024-10-25 13:19:33 +0400

    feat: correct bug with version

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -6,7 +6,7 @@ import { Token } from "@entity";
 
 import configJSON from "@src/config.json";
 
-const CURRENT_CONFIG_VER = "1.5.1";
+const CURRENT_CONFIG_VER = "1.6.0";
 
 function createConfig() {
   assert(configJSON.version === CURRENT_CONFIG_VER, "Version mismatch");
