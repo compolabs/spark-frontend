b4f487c9bdf7cf1d32520a82cdaecb8a3f6b0111
Anton Rock <anton@composabilitylabs.org>
2025-01-16 19:29:50 +0300

    feat: hide pwa modal

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Leaderboard from "src/screens/Leaderboard";
 
 import { Column } from "@components/Flex";
 import Header from "@components/Header";
-import { PWAModal } from "@components/PWAModal";
 import { SplashScreen } from "@components/Splashscreen";
 
 import { useClearUrlParam } from "@hooks/useClearUrlParam";
@@ -51,7 +50,7 @@ const App: React.FC = observer(() => {
                 <Route element={<Leaderboard />} path={ROUTES.LEADERBOARD} />
               </Routes>
               <SideManageAssets />
-              <PWAModal />
+              {/*<PWAModal />*/}
               <SplashScreen />
               <ConnectWalletDialog visible={modalStore.isOpen(MODAL_TYPE.CONNECT_MODAL)} onClose={modalStore.close} />
             </Root>
